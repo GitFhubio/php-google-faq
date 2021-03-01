8c59b4c6dc0e7133094e8ca314542ab730b4938c
Fabio Petrone <fa610movic90@gmail.com>
2021-03-01 15:56:28 +0100

    creati mixins variables anche forzati giusto per rivedere un po' sass

--- index.php
+++ index.php
@@ -4,64 +4,64 @@
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>google</title>
-    <link rel="stylesheet" href="dist/app.css">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-      <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-  </head>
-  <body>
-<div class="header">
-  <div class="header-top">
-    <ul>
-      <li>  <img class="logo" src="img/Google-Logo.png" alt=""></li>
-      <li class="privacyterms">Privacy & Terms</li>
-    </ul>
-<ul>
-  <li><i class="fas fa-ellipsis-v"></i><i class="fas fa-ellipsis-v"></i><i class="fas fa-ellipsis-v"></i></li>
-  <li><span class="user">F</span></li>
-</ul>
+<head>
+  <meta charset="utf-8">
+  <title>google</title>
+  <link rel="stylesheet" href="dist/app.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="header">
+    <div class="header-top">
+      <ul>
+        <li>  <img class="logo" src="img/Google-Logo.png" alt=""></li>
+        <li class="privacyterms">Privacy & Terms</li>
+      </ul>
+      <ul>
+        <li><i class="fas fa-ellipsis-v"></i><i class="fas fa-ellipsis-v"></i><i class="fas fa-ellipsis-v"></i></li>
+        <li><span class="user">F</span></li>
+      </ul>
+    </div>
+    <div class="header-bottom">
+      <ul class="nav">
+        <li>Overview</li>
+        <li>Privacy Policy</li>
+        <li>Terms of Service</li>
+        <li>Technologies</li>
+        <li>FAQ</li>
+      </ul>
+    </div>
   </div>
-  <div class="header-bottom">
-<ul class="nav">
-  <li>Overview</li>
-  <li>Privacy Policy</li>
-  <li>Terms of Service</li>
-  <li>Technologies</li>
-  <li>FAQ</li>
-</ul>
-  </div>
-</div>
-<div class="main container">
-<?php foreach ($faqs as $faq) { ?>
-<ul>
-  <li class="question"><?php echo $faq["question"]; ?></li>
-  <li class="answer"><?php echo $faq["answer"];
-  ?>
+  <div class="main container">
+    <?php foreach ($faqs as $faq) { ?>
+      <ul>
+        <li class="question"><?php echo $faq["question"]; ?></li>
+        <li class="answer"><?php echo $faq["answer"];
+        ?>
 
-  </li>
-</ul>
-<?php } ?>
+      </li>
+    </ul>
+  <?php } ?>
 </div>
 <div class="footer">
-<div class="container">
-  <ul class="footer-left">
-<?php foreach ($footerList as $element) {
-?> <li><?php echo $element; ?></li>
-<?php } ?>
-</ul>
-<ul class="footer-right">
-  <i class="fas fa-globe"></i>
-  <select class="my-select">
-    <?php foreach ($footerSelect as $option) {
-    ?> <option value=""><?php echo $option; ?></option>
-    <?php } ?>
-  </select>
-</ul>
+  <div class="container">
+    <ul class="footer-left">
+      <?php foreach ($footerList as $element) {
+        ?> <li><?php echo $element; ?></li>
+      <?php } ?>
+    </ul>
+    <ul class="footer-right">
+      <i class="fas fa-globe"></i>
+      <select class="my-select">
+        <?php foreach ($footerSelect as $option) {
+          ?> <option value=""><?php echo $option; ?></option>
+        <?php } ?>
+      </select>
+    </ul>
 
+  </div>
 </div>
-</div>
-  </body>
+</body>
 </html>
